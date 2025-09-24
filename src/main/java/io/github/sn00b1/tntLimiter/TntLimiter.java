497a28380e43b5e433b2a0a6aea774e7960b2247
S-N00B-1 <49575478+S-N00B-1@users.noreply.github.com>
2025-09-23 23:50:27 -0700

    Add Icon & commands

--- src/main/java/io/github/sn00b1/tntLimiter/TntLimiter.java
+++ src/main/java/io/github/sn00b1/tntLimiter/TntLimiter.java
@@ -1,7 +1,11 @@
 package io.github.sn00b1.tntLimiter;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import io.github.sn00b1.tntLimiter.config.TntLimiterConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.Commands;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,5 +17,22 @@ public class TntLimiter implements ModInitializer {
     public void onInitialize() {
         TntLimiterConfig.HANDLER.load();
         LOGGER.info("Loaded TNT Limiter Config");
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
+                dispatcher.register(Commands.literal("tntlimiter").requires(source -> source.hasPermission(2))
+                .then(Commands.literal("DetectionRadius")
+                        .then(Commands.argument("radius", IntegerArgumentType.integer(1))
+                                .executes(context -> {
+                                    TntLimiterConfig.DetectionRadius = IntegerArgumentType.getInteger(context, "radius");
+                                    TntLimiterConfig.HANDLER.save();
+                                    return Command.SINGLE_SUCCESS;
+                                })))
+                .then(Commands.literal("TntLimit")
+                        .then(Commands.argument("limit", IntegerArgumentType.integer(0))
+                                .executes(context -> {
+                                    TntLimiterConfig.TntLimit = IntegerArgumentType.getInteger(context, "limit");
+                                    TntLimiterConfig.HANDLER.save();
+                                    return Command.SINGLE_SUCCESS;
+                                })))));
     }
 }
